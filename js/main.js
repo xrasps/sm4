9ad36477f6974fe673ce7d7d7c6f24bf7f3bbb8e
xrasps <evelina.gazetdinova2003@gmail.com>
2023-09-09 14:49:14 -0300

    add link

--- js/main.js
+++ js/main.js
@@ -3,5 +3,4 @@ let cityAge = +prompt('Год образования города?', '');
 let cityQuantity = prompt('Численность населения?', '');
 let age = 2023 - cityAge;
 alert('Городу ' + cityName + ' исполнилось ' + age + ' лет с момента его образования. Население - ' + cityQuantity + ' человек');
-
-alert("ссылка на гит - ")
+alert("ссылка на гит -  https://github.com/xrasps/sm2.git")
