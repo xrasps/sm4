4f5f6a8bae3a86a77e2de730fbb61185b48e2dcf
xrasps <evelina.gazetdinova2003@gmail.com>
2023-09-18 14:59:18 +0300

    ADD

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
-let cityName = prompt('Ваш город?', '');
-let cityAge = +prompt('Год образования города?', '');
-let cityQuantity = prompt('Численность населения?', '');
-let age = 2023 - cityAge;
-alert('Городу ' + cityName + ' исполнилось ' + age + ' лет с момента его образования. Население - ' + cityQuantity + ' человек');
-alert("ссылка на гит -  https://github.com/xrasps/sm2.git")
+alert("Давайте сделаем покупку")
+let oneNum = +prompt('стоимость товара?','')
+let twoNum = +prompt('ваше количество денег?','')
+let falta;
+const result = document.querySelector('.content');
+if(oneNum == twoNum){
+    result.innerHTML = 'покупка совершена!'
+}else if(oneNum > twoNum){
+    falta = oneNum - twoNum;
+    result.innerHTML = 'вам не хватает ' + falta +  'руб';
+}else{
+    falta = twoNum - oneNum;
+    result.innerHTML = 'покупка совершена. Сдача ' + falta + ' руб';
+}
+
+// css edit
+result.style.background = '#232323';
+result.style.fontSize = '20px';
+result.style.borderRadius = '5px';
+result.style.color = '#ffffe7';
